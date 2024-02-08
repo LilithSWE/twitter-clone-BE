227b6e100efa07eada477868191589e921af75ad
Amanda Hansson <amanda9307@gmail.com>
2024-02-08 11:21:32 +0100

    router for msg

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var msgRouter = require('./routes/message');
 
 var app = express();
 
@@ -17,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/msg', msgRouter);
 
 const MongoClient = require('mongodb').MongoClient;
 
